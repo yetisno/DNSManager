01035664fb1e1843a381a2552c0dae85ee9ba5f0
ant <ant@git.yetiz.org>
2015-02-01 15:53:18 +0800

    main page

---
+++ skel/config.rb
@@ -0,0 +1,14 @@
+require "susy"
+require "breakpoint"
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "img"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
