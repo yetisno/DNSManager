7f079f64ba9bf7e9a49934662fbe66df08dae73f
Yeti Sno <yeti@yetiz.org>
2015-02-23 01:36:14 +0800

    update admin/domain#index add tooltip js invoke add constraint of priority to mxes

--- app/models/mx.rb
+++ app/models/mx.rb
@@ -2,6 +2,6 @@ class Mx < ActiveRecord::Base
 	belongs_to :domain
 	validates :domain_id, presence: true
 	validates :name, presence: true
-	validates :priority, presence: true
+	validates :priority, presence: true, numericality: :only_integer
 	validates :to_name, presence: true
 end
